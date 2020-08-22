29d109049bec6c03b58fa409ff3dbc53d8d2861b
zhanghaifeng2019 <zhang@rentracks.co.jp>
2020-08-22 16:27:45 +0900

    good

--- server/src/config/dev.js
+++ server/src/config/dev.js
@@ -3,5 +3,7 @@
 module.exports={
     googleClientID:"708760631929-bc0ra1hftbmga3jb59te0o1t5ptq5tev.apps.googleusercontent.com",
     googleClientSecret:"zfTfZYQThUDpmcQH2eex09cS",
-    mongoURI:"mongodb+srv://zhanghaifeng:no744634@ec-site-test-dev.hktke.mongodb.net/ec-site-test-dev?retryWrites=true&w=majority"
+    mongoURI:"mongodb+srv://zhanghaifeng:no744634@ec-site-test-dev.hktke.mongodb.net/ec-site-test-dev?retryWrites=true&w=majority",
+    stripePublishableKey:"pk_test_51HIm9lDcLy1amPKtsNQrtgHbhBqLTqtulcRFGU9rcuXLtbsFVy8QXKhDg8MaHJ1UgQxO6Gplj0ZYNGJO8Rukbx3600LeLh6bnY",
+    stripeSecretKey:"sk_test_51HIm9lDcLy1amPKtrqVXovF9kbgIe25sMLRXmRKa9204cHj085UnFM5T55vtMLPLDKaCZGsDvvkzFmzNULWZ1GzT00lKqbh7Th",
 }
--- server/src/config/prd.js
+++ server/src/config/prd.js
@@ -2,7 +2,9 @@
 module.exports={
     googleClientID: "473877805750-ro2n4ae78jlqqqegoe0mhra15n5jo53m.apps.googleusercontent.com",
     googleClientSecret:"i850VbtBbaAts-vhx-RYp-Sc",
-    mongoURI:"mongodb+srv://zhanghaifeng:no744634@ec-site-test-product.ynisc.mongodb.net/ec-site-test-product?retryWrites=true&w=majority"
+    mongoURI:"mongodb+srv://zhanghaifeng:no744634@ec-site-test-product.ynisc.mongodb.net/ec-site-test-product?retryWrites=true&w=majority",
+    stripePublishableKey:"pk_test_51HIm9lDcLy1amPKtsNQrtgHbhBqLTqtulcRFGU9rcuXLtbsFVy8QXKhDg8MaHJ1UgQxO6Gplj0ZYNGJO8Rukbx3600LeLh6bnY",
+    stripeSecretKey:"sk_test_51HIm9lDcLy1amPKtrqVXovF9kbgIe25sMLRXmRKa9204cHj085UnFM5T55vtMLPLDKaCZGsDvvkzFmzNULWZ1GzT00lKqbh7Th",
 }
 
 
